b53ff68661d5362d0745294efab571cade0b47ce
OmarFaig <omerfaiq7@gmail.com>
2024-06-25 11:44:30 +0200

    new page

---
+++ index.js
@@ -0,0 +1,10 @@
+const container = document.getElementById('container');
+const flashlight = document.querySelector('.flashlight');
+
+const followMouseFlashlight = ({ clientX, clientY }) => {
+    const { left, top } = container.getBoundingClientRect();
+    flashlight.style.left = `${clientX - left - 175}px`; // Adjust to center the light
+    flashlight.style.top = `${clientY - top - 175}px`;  // Adjust to center the light
+};
+
+container.addEventListener('mousemove', followMouseFlashlight);
